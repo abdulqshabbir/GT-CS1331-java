910807d4f6452cdfda66db41803b2423ea55ea1b
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-07-22 12:40:57 +0800

    closed Scanner input stream

--- module_1/FarToCel.java
+++ module_1/FarToCel.java
@@ -8,5 +8,6 @@ public class FarToCel {
         int far_value = input.nextInt();
         double cel_value = (5.0/9)*(far_value - 32);
         System.out.print("The temperature in celcius is: " + cel_value);
+        input.close();
     }
 }
