8c8a84263a6e130e7ac3b67b8d62d054b24c524b
Abdul Shabbir <abdulqshabbir@gmail.com>
2021-07-30 15:28:53 +0800

    small change

--- HW04/Pond.java
+++ HW04/Pond.java
@@ -5,9 +5,9 @@ public class Pond {
         Frog peepaw = new Frog("Peepaw", 4.6, 5.0);
         Frog abdul = new Frog("Abdul", 29.0, 15.0);
 
-        Fly fly1 = new Fly(1.0, 3.0);
+        new Fly(1.0, 3.0);
         Fly fly2 = new Fly(6.0);
-        Fly fly3 = new Fly(5.0);
+        new Fly(5.0);
 
         Frog.setSpecies("1331 Frogs");
         System.out.println(peepo);
